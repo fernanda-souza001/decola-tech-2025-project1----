75d870bd865b92b61f72a729c48e58733df7eab3
Fernanda Dias Marques de Souza <fernanda.souza@mottu.com.br>
2025-02-22 10:34:58 -0300

    Initial commit

---
+++ src/main/java/dev/fernanda/decola_tech_2025_project1/DecolaTech2025Project1Application.java
@@ -0,0 +1,13 @@
+package dev.fernanda.decola_tech_2025_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecolaTech2025Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecolaTech2025Project1Application.class, args);
+	}
+
+}
---
+++ src/test/java/dev/fernanda/decola_tech_2025_project1/DecolaTech2025Project1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.fernanda.decola_tech_2025_project1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecolaTech2025Project1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
